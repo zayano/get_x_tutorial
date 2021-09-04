b99308015b47e8a0df442501d266a47fb8c9c1e2
Reza Aditya Sutrino <adityareza46@gmail.com>
2021-09-04 13:45:19 +0700

    update app_widget

--- lib/presentation/app_widget.dart
+++ lib/presentation/app_widget.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_x_tutorial/presentation/home_page.dart';
+import 'package:get_x_tutorial/presentation/dashboard/dashboard_page.dart';
+import 'package:get_x_tutorial/presentation/setting/setting_page.dart';
+import 'package:get_x_tutorial/presentation/sign_in/sign_in_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter GetX',
+      getPages: [
+        GetPage(
+            name: '/',
+            page: () => SignInPage(),
+            transition: Transition.cupertino),
+        GetPage(
+            name: '/dashboard',
+            page: () => DashboardPage(),
+            transition: Transition.cupertino),
+        GetPage(
+            name: '/setting ',
+            page: () => SettingPage(),
+            transition: Transition.cupertino),
+      ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      initialRoute: '/',
     );
   }
 }
